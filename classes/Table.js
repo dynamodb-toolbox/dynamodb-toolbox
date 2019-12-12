1ae8d05fad9ae711fc28b8495f109e12e3b04f4d
Jeremy Daly <jeremy@jeremydaly.com>
2019-12-12 18:39:16 -0500

    break up components (initial)

--- classes/Table.js
+++ classes/Table.js
@@ -3,15 +3,9 @@
 /**
  * DynamoDB Toolbox: A simple set of tools for working with Amazon DynamoDB
  * @author Jeremy Daly <jeremy@jeremydaly.com>
- * @version 0.2.0
  * @license MIT
  */
 
-const Model = require('./classes/Model')
-
-module.exports = {
-  Model
-}
-
-
-// TODO: prevent reserved field names?
+ class Table {
+   constructor() {}
+ }
