6e9c3b58414f84a83e4558c61920198014dea2e2
Jeremy Daly <jeremy@jeremydaly.com>
2020-07-10 11:50:52 -0400

    close #66 by adding entity type alias mapping to attributes

--- classes/Entity.js
+++ classes/Entity.js
@@ -53,12 +53,12 @@ class Entity {
       // Set the Entity's table
       this._table = table
       
-      // If an entity tracking field is enabled, add the attribute and the default
+      // If an entity tracking field is enabled, add the attributes, alias and the default
       if (table.Table.entityField) {
-        this.schema.attributes[table.Table.entityField] = { 
-          type: 'string', alias: 'entity', default: this.name
-        }
+        this.schema.attributes[table.Table.entityField] = { type: 'string', alias: this._etAlias, default: this.name }
         this.defaults[table.Table.entityField] = this.name
+        this.schema.attributes[this._etAlias] = { type: 'string', map: table.Table.entityField, default: this.name }
+        this.defaults[this._etAlias] = this.name
       } // end if entity tracking
     
     // Throw an error if not a valid Table
