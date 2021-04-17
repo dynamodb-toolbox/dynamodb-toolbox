735970b335655fe867c693d6e1a6fbadc9659d0c
Jake Maldonado <jake@Jakes-MacBook-Pro.local>
2021-04-17 13:22:14 -0400

    fix required field check not accepting 0 and false values

--- src/classes/Entity.ts
+++ src/classes/Entity.ts
@@ -1110,7 +1110,7 @@ class Entity<
 
     // Check for required fields
     Object.keys(required).forEach(field => 
-      required[field] !== undefined && !data[field] 
+      required[field] !== undefined && data[field] !== null
         && error(`'${field}${this.schema.attributes[field].alias ? `/${this.schema.attributes[field].alias}` : ''}' is a required field`)
     ) // end required field check
 
