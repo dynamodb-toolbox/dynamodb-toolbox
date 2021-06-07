622cb7e8ad1beb66792d15efea1443c68438a3ec
Denis Zatsepin <denis@zatsepin.me>
2021-06-07 11:24:15 +0200

    refactor: set validator

--- src/lib/validateTypes.ts
+++ src/lib/validateTypes.ts
@@ -8,8 +8,11 @@ import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb'
 import { toBool, hasValue, error } from './utils'
 
 // Performs type validation/coercian
-export default (DocumentClient: DynamoDBDocumentClient) => (mapping: any, field: any, value: any) => {
-
+export default (DocumentClient: DynamoDBDocumentClient) => (
+  mapping: any,
+  field: any,
+  value: any
+) => {
   // Evaluate function expressions
   // TODO: should this happen here?
   // let value = typeof input === 'function' ? input(data) : input
@@ -17,42 +20,94 @@ export default (DocumentClient: DynamoDBDocumentClient) => (mapping: any, field:
   // return if undefined or null
   if (!hasValue(value)) return value
 
-  switch(mapping.type) {
+  switch (mapping.type) {
     case 'string':
-      return typeof value === 'string' || mapping.coerce ? String(value)
+      return typeof value === 'string' || mapping.coerce
+        ? String(value)
         : error(`'${field}' must be of type string`)
     case 'boolean':
-      return typeof value === 'boolean' || mapping.coerce ? toBool(value)
+      return typeof value === 'boolean' || mapping.coerce
+        ? toBool(value)
         : error(`'${field}' must be of type boolean`)
-    case 'number':    
-      return typeof value === 'number' || mapping.coerce ?
-        (String(parseInt(value)) === String(value) ? parseInt(value)
-        : String(parseFloat(value)) === String(value) ? parseFloat(value)
-        : error(`Could not convert '${value}' to a number for '${field}'`))
+    case 'number':
+      return typeof value === 'number' || mapping.coerce
+        ? String(parseInt(value)) === String(value)
+          ? parseInt(value)
+          : String(parseFloat(value)) === String(value)
+          ? parseFloat(value)
+          : error(`Could not convert '${value}' to a number for '${field}'`)
         : error(`'${field}' must be of type number`)
     case 'list':
-      return Array.isArray(value) ? value
-        : mapping.coerce ? String(value).split(',').map(x => x.trim())
+      return Array.isArray(value)
+        ? value
+        : mapping.coerce
+        ? String(value)
+            .split(',')
+            .map((x) => x.trim())
         : error(`'${field}' must be a list (array)`)
     case 'map':
-      return typeof value === 'object' && !Array.isArray(value) ? value
+      return typeof value === 'object' && !Array.isArray(value)
+        ? value
         : error(`'${field}' must be a map (object)`)
     case 'set':
-      // if (Array.isArray(value)) {
-      //   if (!DocumentClient) error('DocumentClient required for this operation')
-      //   let set = DocumentClient.createSet(value, { validate: true })
-      //   return !mapping.setType || mapping.setType === set.type.toLowerCase() ? set
-      //     : error(`'${field}' must be a valid set (array) containing only ${mapping.setType} types`)
-      // } else if (mapping.coerce) {
-      //   if (!DocumentClient) error('DocumentClient required for this operation')
-      //   let set = DocumentClient.createSet(String(value).split(',').map(x => x.trim()))
-      //   return !mapping.setType || mapping.setType === set.type.toLowerCase() ? set
-      //     : error(`'${field}' must be a valid set (array) of type ${mapping.setType}`)
-      // } else {
-      //   return error(`'${field}' must be a valid set (array)`)
-      // }
+      if (value instanceof Set) {
+        return hasSameTypes([...value])
+          ? value
+          : error(
+              `'${field}' must be a valid set containing only ${mapping.setType} types`
+            )
+      } else if (Array.isArray(value)) {
+        return (!mapping.setType ||
+          value.length === 0 ||
+          mapping.setType === typeOf(value[0]).toLowerCase()) &&
+          hasSameTypes(value)
+          ? new Set(value)
+          : error(
+              `'${field}' must be a valid set (array) containing only ${mapping.setType} types`
+            )
+      } else if (mapping.coerce) {
+        const arrayVal = String(value)
+          .split(',')
+          .map((x) => x.trim())
+        return (!mapping.setType ||
+          arrayVal.length === 0 ||
+          mapping.setType === typeOf(arrayVal[0]).toLowerCase()) &&
+          hasSameTypes(arrayVal)
+          ? new Set(arrayVal)
+          : error(
+              `'${field}' must be a valid set (array) of type ${mapping.setType}`
+            )
+      } else {
+        return error(`'${field}' must be a valid set (array)`)
+      }
     default:
       // TODO: Binary validation
       return value
   }
 } // end validateTypes
+
+function hasSameTypes<T>(array: Array<T>): boolean {
+  const length = array.length
+  if (length <= 1) {
+    return true
+  }
+  const firstType = typeOf(array[0])
+
+  return array.slice(1).every((el: T) => typeOf(el) === firstType)
+}
+
+function typeOf(data: any) {
+  if (data === null && typeof data === 'object') {
+    return 'null'
+  } else if (data !== undefined && Buffer.isBuffer(data)) {
+    return 'Binary'
+  } else if (data !== undefined && data.constructor) {
+    return data.wrapperName || data.constructor.name
+  } else if (data !== undefined && typeof data === 'object') {
+    // this object is the result of Object.create(null), hence the absence of a
+    // defined constructor
+    return 'Object'
+  } else {
+    return 'undefined'
+  }
+}
