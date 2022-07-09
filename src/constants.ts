842db68a924793298513a8908e2a1c89fc1fe791
Naor Peled <thehecticbyte@gmail.com>
2022-07-09 20:26:10 +0300

    cleanup

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,2 @@
-export const UPDATE_EXPRESSION_ATTRIBUTE_LIST = {
-  ':ddb_toolbox_default_list_value': [],
-}
-
 export const UPDATE_EXPRESSION_VALUES_LIST_DEFAULT_KEY = ':_ld'
 export const UPDATE_EXPRESSION_VALUES_LIST_DEFAULT_VALUE = []
