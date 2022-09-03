1591454c209afbb6f6e37ef7c8d6baec41f637bf
ThomasAribart <thomasa@theodo.fr>
2022-09-03 12:12:03 +0200

    improve test & build definition process

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  testMatch: ['**/?(*.)+(unit.test).+(ts|tsx|js)'],
   testPathIgnorePatterns: ['/__tests__/entities/*', '/__tests__/tables/*'],
   coveragePathIgnorePatterns: ['/__tests__/*'],
   transform: {
