b92b66ba5a641d4ba6a35c764be0e98fc442e626
ThomasAribart <thomasa@theodo.fr>
2022-09-15 22:37:33 +0200

    enable key elements in lists

--- src/v1/item/typers/list/types.ts
+++ src/v1/item/typers/list/types.ts
@@ -4,7 +4,6 @@ import type { Property } from '../types/property'
 export type ListProperty = Property & {
   _required: AtLeastOnce
   _hidden: false
-  _key: false
   _savedAs: undefined
   _default: undefined
 }
