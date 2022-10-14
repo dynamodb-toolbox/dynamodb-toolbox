d0f4b525987e201a58cdfb50e706fbb8cecafdbe
ThomasAribart <thomasa@theodo.fr>
2022-10-14 20:52:29 +0200

    rename attributes

--- src/v1/item/typers/leaf/index.ts
+++
@@ -1,9 +0,0 @@
-export { string, boolean, binary, number } from './typer'
-export type { LeafType, ResolveLeafType, ResolvedLeafType } from './types'
-export type { Leaf } from './interface'
-export {
-  validateLeaf,
-  InvalidEnumValueTypeError,
-  InvalidDefaultValueTypeError,
-  InvalidDefaultValueRangeError
-} from './validate'
