ac6acd8f3ae8269d3f8f42b65c18f25d81c8a0d2
Naor Peled <thehecticbyte@gmail.com>
2022-11-05 03:47:30 +0200

    .

--- src/classes/Table/Table.ts
+++ src/classes/Table/Table.ts
@@ -717,8 +717,7 @@ class Table<Name extends string, PartitionKey extends A.Key, SortKey extends A.K
           result,
           {
             Items: result.Items?.map((item) => {
-              const isEntityFieldEnabledOnTable = this.Table.entityField !== false;
-              const itemEntityName = item[isEntityFieldEnabledOnTable ? this.table.entityField : undefined] || options.entity;
+              const itemEntityName = item[this.Table.entityField !== false ? this.Table.entityField : undefined as never] || options.entity;
               const itemEntityInstance = this[itemEntityName]
 
               if (itemEntityInstance != null) {
