e3274c9607fd69a8b3ea3b3b0b2e21a0a503745c
Naor Peled <thehecticbyte@gmail.com>
2022-12-05 01:08:48 +0200

    .

--- src/classes/Entity/Entity.ts
+++ src/classes/Entity/Entity.ts
@@ -1617,7 +1617,7 @@ class Entity<Name extends string = string,
       return this as any
     }
 
-    if(!(table instanceof Table)) {
+    if(!table.Table.attributes) {
       error(`Entity ${this.name} was assigned an invalid table`)
     }
 
