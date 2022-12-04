8a247eb13a65f1d6722c2d2e696476901eebc526
Thomas Aribart <38014240+ThomasAribart@users.noreply.github.com>
2022-12-04 10:09:55 +0100

    Use attributes freezing (#378)

--- src/v1/entity/generics/KeyInput.ts
+++ src/v1/entity/generics/KeyInput.ts
@@ -1,13 +1,20 @@
 import type { O } from 'ts-toolbelt'
 
-import type { Item } from 'v1/item/interface'
 import type {
+  FrozenItem,
+  Item,
+  FrozenAttribute,
   Attribute,
   ResolvedAttribute,
+  FrozenAnyAttribute,
   AnyAttribute,
+  FrozenLeafAttribute,
   LeafAttribute,
+  FrozenSetAttribute,
   SetAttribute,
+  FrozenListAttribute,
   ListAttribute,
+  FrozenMapAttribute,
   MapAttribute,
   Always
 } from 'v1/item'
@@ -20,22 +27,57 @@ import { EntityV2 } from '../class'
  * @param Input Entity | Item | Attribute
  * @return Object
  */
-export type KeyInput<Input extends EntityV2 | Item | Attribute> = Input extends AnyAttribute
+export type KeyInput<
+  Input extends EntityV2 | FrozenItem | FrozenAttribute
+> = Input extends FrozenAnyAttribute
+  ? ResolvedAttribute
+  : Input extends FrozenLeafAttribute
+  ? NonNullable<Input['resolved']>
+  : Input extends FrozenSetAttribute
+  ? Set<KeyInput<Input['elements']>>
+  : Input extends FrozenListAttribute
+  ? KeyInput<Input['elements']>[]
+  : Input extends FrozenMapAttribute | FrozenItem
+  ? O.Required<
+      O.Partial<
+        {
+          // Keep only key attributes
+          [key in O.SelectKeys<Input['attributes'], { key: true }>]: KeyInput<
+            Input['attributes'][key]
+          >
+        }
+      >,
+      Exclude<
+        // Enforce Always Required attributes
+        O.SelectKeys<Input['attributes'], { required: Always }>,
+        // ...Except those that have default (not required from user, can be provided by the lib)
+        O.FilterKeys<Input['attributes'], { default: undefined }>
+      >
+    > & // Add Record<string, ResolvedAttribute> if map is open
+      (Input extends { open: true } ? Record<string, ResolvedAttribute> : {})
+  : Input extends EntityV2
+  ? KeyInput<Input['frozenItem']>
+  : never
+
+// TODO: Required in Entity constructor... See if possible to use only KeyInput w. FrozenItem
+export type _KeyInput<Input extends EntityV2 | Item | Attribute> = Input extends AnyAttribute
   ? ResolvedAttribute
   : Input extends LeafAttribute
   ? NonNullable<Input['_resolved']>
   : Input extends SetAttribute
-  ? Set<KeyInput<Input['_elements']>>
+  ? Set<_KeyInput<Input['_elements']>>
   : Input extends ListAttribute
-  ? KeyInput<Input['_elements']>[]
+  ? _KeyInput<Input['_elements']>[]
   : Input extends MapAttribute | Item
   ? O.Required<
-      O.Partial<{
-        // Keep only key attributes
-        [key in O.SelectKeys<Input['_attributes'], { _key: true }>]: KeyInput<
-          Input['_attributes'][key]
-        >
-      }>,
+      O.Partial<
+        {
+          // Keep only key attributes
+          [key in O.SelectKeys<Input['_attributes'], { _key: true }>]: _KeyInput<
+            Input['_attributes'][key]
+          >
+        }
+      >,
       Exclude<
         // Enforce Always Required attributes
         O.SelectKeys<Input['_attributes'], { _required: Always }>,
@@ -45,5 +87,5 @@ export type KeyInput<Input extends EntityV2 | Item | Attribute> = Input extends 
     > & // Add Record<string, ResolvedAttribute> if map is open
       (Input extends { _open: true } ? Record<string, ResolvedAttribute> : {})
   : Input extends EntityV2
-  ? KeyInput<Input['item']>
+  ? _KeyInput<Input['item']>
   : never
