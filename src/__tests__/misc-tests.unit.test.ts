e5962d9cb56aaedf4beafd0fd04f2c965341e788
Clement Marcilhacy <clementmarcilhacy@gmail.com>
2022-12-09 17:08:02 +0100

    feat: remove unit in test command and remove clg

--- src/__tests__/misc-tests.unit.test.ts
+++ src/__tests__/misc-tests.unit.test.ts
@@ -23,7 +23,7 @@ describe('Misc Tests (development only)', () => {
       table
     } as const)
 
-    console.log(TestEntity.schema)
+    // console.log(TestEntity.schema)
 
     // console.log(
     //   await TestEntity.query(1)
