21f9f546c0738e6ea428f2914a8b57712a0d7ac3
ThomasAribart <thomasa@theodo.fr>
2022-12-20 09:52:06 +0100

    rename ResolvedPrimitiveAttributeType to ResolvedPrimitiveAttribute and export PossiblyUndefinedResolvedAttribute types

--- src/v1/utils/marshall/marshall.ts
+++ src/v1/utils/marshall/marshall.ts
@@ -1,14 +1,9 @@
 import type { AttributeValue } from '@aws-sdk/client-dynamodb'
 import { marshall as dynamodbMarshall } from '@aws-sdk/util-dynamodb'
 
-import { PossiblyUndefinedResolvedAttribute } from 'v1/item'
+import { PossiblyUndefinedResolvedItem } from 'v1/item'
 
 import { filterUndefinedValues } from './filterUndefinedValues'
 
-export const marshall = <
-  ITEM extends {
-    [key: string]: PossiblyUndefinedResolvedAttribute
-  }
->(
-  item: ITEM
-): Record<string, AttributeValue> => dynamodbMarshall(filterUndefinedValues(item))
+export const marshall = (item: PossiblyUndefinedResolvedItem): Record<string, AttributeValue> =>
+  dynamodbMarshall(filterUndefinedValues(item))
