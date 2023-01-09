293c1fdfe2ccfa79e03011c9ce06cfb0830e3c8a
ThomasAribart <thomasa@theodo.fr>
2023-01-09 19:25:53 +0100

    first proposal for getItem command (not tested)

---
+++ src/v1/commands/getItem/getItemParams/parseKeyInput/set.ts
@@ -0,0 +1,22 @@
+import { SetAttribute, PossiblyUndefinedResolvedAttribute, PutItem } from 'v1'
+import { isSet } from 'v1/utils/validation'
+
+import { parseAttributeKeyInput } from './attribute'
+
+export const parseSetAttributeKeyInput = <SET_ATTRIBUTE extends SetAttribute>(
+  setAttribute: SET_ATTRIBUTE,
+  input: PossiblyUndefinedResolvedAttribute
+): PutItem<SET_ATTRIBUTE> => {
+  if (!isSet(input)) {
+    // TODO
+    throw new Error()
+  }
+
+  const parsedPutItemInput: PossiblyUndefinedResolvedAttribute = new Set()
+
+  input.forEach(element =>
+    parsedPutItemInput.add(parseAttributeKeyInput(setAttribute.elements, element))
+  )
+
+  return parsedPutItemInput as PutItem<SET_ATTRIBUTE>
+}
