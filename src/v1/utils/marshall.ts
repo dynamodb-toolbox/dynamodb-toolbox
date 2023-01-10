135868dbbeed72356b82595e68702748d6b4efcc
ThomasAribart <thomasa@theodo.fr>
2023-01-10 22:33:02 +0100

    use removeUndefinedValues option instead of custom filterUndefinedValues util

--- src/v1/utils/marshall.ts
+++ src/v1/utils/marshall.ts
@@ -3,7 +3,9 @@ import { marshall as dynamodbMarshall } from '@aws-sdk/util-dynamodb'
 
 import { PossiblyUndefinedResolvedItem } from 'v1/item'
 
-import { filterUndefinedValues } from './filterUndefinedValues'
-
 export const marshall = (item: PossiblyUndefinedResolvedItem): Record<string, AttributeValue> =>
-  dynamodbMarshall(filterUndefinedValues(item))
+  dynamodbMarshall(item, {
+    convertEmptyValues: false,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: false
+  })
