31b142ff355f8a80772bffe8cb860bf182952e64
ThomasAribart <thomasa@theodo.fr>
2023-01-10 22:33:02 +0100

    use removeUndefinedValues option instead of custom filterUndefinedValues util

--- src/v1/utils/marshall/definedValuesKeys.type.test.ts
+++
@@ -1,7 +0,0 @@
-import type { A } from 'ts-toolbelt'
-
-import type { DefinedValuesKeys } from './definedValuesKeys'
-import type { ObjectWithUndefinedValues } from './filterUndefinedValues.fixtures.test'
-
-const assertDefinedValuesKeys: A.Equals<DefinedValuesKeys<ObjectWithUndefinedValues>, 'b' | 'c'> = 1
-assertDefinedValuesKeys
