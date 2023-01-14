df1ae09491377c5b9b78f7f0ac03ba530ffd827f
ThomasAribart <thomasa@theodo.fr>
2023-01-14 16:14:09 +0100

    create DefaultsComputer type

---
+++ src/v1/entity/generics/_DefaultsComputer/index.ts
@@ -0,0 +1,3 @@
+export type { ItemDefaultsComputer, AttributeDefaultsComputer } from './types'
+export type { _ItemPutDefaultsComputer } from './item'
+export type { _AttributePutDefaultsComputer } from './attribute'
