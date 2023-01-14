7a5617fb5a8e7001607a26b45a9907208006c54d
ThomasAribart <thomasa@theodo.fr>
2023-01-14 16:14:09 +0100

    create DefaultsComputer type

---
+++ src/v1/entity/generics/_DefaultsComputer/item.ts
@@ -0,0 +1,20 @@
+import type { _Item } from 'v1/item'
+import type { OmitUndefinedProperties } from 'v1/types'
+
+import type { _PutItemInput } from '../PutItemInput'
+
+import type { _AttributePutDefaultsComputer } from './attribute'
+
+export type _ItemPutDefaultsComputer<
+  _ITEM extends _Item,
+  _ATTRIBUTES_PUT_DEFAULT_COMPUTERS = OmitUndefinedProperties<
+    {
+      [KEY in keyof _ITEM['_attributes']]: _AttributePutDefaultsComputer<
+        _ITEM['_attributes'][KEY],
+        [_PutItemInput<_ITEM, true>]
+      >
+    }
+  >
+> = keyof _ATTRIBUTES_PUT_DEFAULT_COMPUTERS extends never
+  ? undefined
+  : { [KEY in keyof _ATTRIBUTES_PUT_DEFAULT_COMPUTERS]: _ATTRIBUTES_PUT_DEFAULT_COMPUTERS[KEY] }
