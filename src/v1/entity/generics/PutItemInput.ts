1741033c0876e6f2969f825c9d7ad17f85f9da47
ThomasAribart <thomasa@theodo.fr>
2023-01-16 20:19:46 +0100

    improve some naming

--- src/v1/entity/generics/PutItemInput.ts
+++ src/v1/entity/generics/PutItemInput.ts
@@ -27,21 +27,21 @@ import type { EntityV2 } from '../class'
 /**
  * User input of a PUT command for a given Entity, Item or Attribute
  *
- * @param Input Entity | Item | Attribute
- * @param RequireInitialDefaults Boolean
+ * @param Schema Entity | Item | Attribute
+ * @param RequireHardDefaults Boolean
  * @return Object
  */
 export type PutItemInput<
   SCHEMA extends EntityV2 | Item | Attribute,
-  REQUIRE_INITIAL_DEFAULTS extends boolean = false
+  REQUIRE_HARD_DEFAULTS extends boolean = false
 > = SCHEMA extends AnyAttribute
   ? ResolvedAttribute
   : SCHEMA extends PrimitiveAttribute
   ? NonNullable<SCHEMA['resolved']>
   : SCHEMA extends SetAttribute
-  ? Set<PutItemInput<SCHEMA['elements'], REQUIRE_INITIAL_DEFAULTS>>
+  ? Set<PutItemInput<SCHEMA['elements'], REQUIRE_HARD_DEFAULTS>>
   : SCHEMA extends ListAttribute
-  ? PutItemInput<SCHEMA['elements'], REQUIRE_INITIAL_DEFAULTS>[]
+  ? PutItemInput<SCHEMA['elements'], REQUIRE_HARD_DEFAULTS>[]
   : SCHEMA extends MapAttribute | Item
   ? O.Required<
       O.Partial<
@@ -49,7 +49,7 @@ export type PutItemInput<
           // Keep all attributes
           [KEY in keyof SCHEMA['attributes']]: PutItemInput<
             SCHEMA['attributes'][KEY],
-            REQUIRE_INITIAL_DEFAULTS
+            REQUIRE_HARD_DEFAULTS
           >
         }
       >,
@@ -58,28 +58,28 @@ export type PutItemInput<
           SCHEMA['attributes'],
           { required: AtLeastOnce | OnlyOnce | Always; default: undefined }
         >
-      // Add attributes with initial (non-computed) defaults if RequireInitialDefaults is true
-      | (REQUIRE_INITIAL_DEFAULTS extends true
+      // Add attributes with hard (non-computed) defaults if REQUIRE_HARD_DEFAULTS is true
+      | (REQUIRE_HARD_DEFAULTS extends true
           ? O.FilterKeys<SCHEMA['attributes'], { default: undefined | ComputedDefault }>
           : never)
     > & // Add Record<string, ResolvedAttribute> if map is open
       (SCHEMA extends { open: true } ? Record<string, ResolvedAttribute> : {})
   : SCHEMA extends EntityV2
-  ? PutItemInput<SCHEMA['item'], REQUIRE_INITIAL_DEFAULTS>
+  ? PutItemInput<SCHEMA['item'], REQUIRE_HARD_DEFAULTS>
   : never
 
 // TODO: Required in Entity constructor... See if possible to use only PutItemInput
 export type _PutItemInput<
   _SCHEMA extends EntityV2 | _Item | _Attribute,
-  REQUIRE_INITIAL_DEFAULTS extends boolean = false
+  REQUIRE_HARD_DEFAULTS extends boolean = false
 > = _SCHEMA extends _AnyAttribute
   ? ResolvedAttribute
   : _SCHEMA extends _PrimitiveAttribute
   ? NonNullable<_SCHEMA['_resolved']>
   : _SCHEMA extends _SetAttribute
-  ? Set<_PutItemInput<_SCHEMA['_elements'], REQUIRE_INITIAL_DEFAULTS>>
+  ? Set<_PutItemInput<_SCHEMA['_elements'], REQUIRE_HARD_DEFAULTS>>
   : _SCHEMA extends _ListAttribute
-  ? _PutItemInput<_SCHEMA['_elements'], REQUIRE_INITIAL_DEFAULTS>[]
+  ? _PutItemInput<_SCHEMA['_elements'], REQUIRE_HARD_DEFAULTS>[]
   : _SCHEMA extends _MapAttribute | _Item
   ? O.Required<
       O.Partial<
@@ -87,7 +87,7 @@ export type _PutItemInput<
           // Keep all attributes
           [KEY in keyof _SCHEMA['_attributes']]: _PutItemInput<
             _SCHEMA['_attributes'][KEY],
-            REQUIRE_INITIAL_DEFAULTS
+            REQUIRE_HARD_DEFAULTS
           >
         }
       >,
@@ -96,12 +96,12 @@ export type _PutItemInput<
           _SCHEMA['_attributes'],
           { _required: AtLeastOnce | OnlyOnce | Always; _default: undefined }
         >
-      // Add attributes with initial (non-computed) defaults if RequireInitialDefaults is true
-      | (REQUIRE_INITIAL_DEFAULTS extends true
+      // Add attributes with hard (non-computed) defaults if REQUIRE_HARD_DEFAULTS is true
+      | (REQUIRE_HARD_DEFAULTS extends true
           ? O.FilterKeys<_SCHEMA['_attributes'], { _default: undefined | ComputedDefault }>
           : never)
     > & // Add Record<string, ResolvedAttribute> if map is open
       (_SCHEMA extends { _open: true } ? Record<string, ResolvedAttribute> : {})
   : _SCHEMA extends EntityV2
-  ? _PutItemInput<_SCHEMA['_item'], REQUIRE_INITIAL_DEFAULTS>
+  ? _PutItemInput<_SCHEMA['_item'], REQUIRE_HARD_DEFAULTS>
   : never
