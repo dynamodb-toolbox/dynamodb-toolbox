917c67b5054b8f3b545138625d7293e2fae10ed6
ThomasAribart <thomasa@theodo.fr>
2023-01-27 19:46:52 +0100

    use named props instead of ordered args in any attribute

--- src/v1/item/attributes/any/options.ts
+++ src/v1/item/attributes/any/options.ts
@@ -1,31 +1,45 @@
-import type { AttributeOptions } from '../shared/options'
-import type { RequiredOption, AtLeastOnce } from '../constants/requiredOptions'
+import type { RequiredOption } from '../constants/requiredOptions'
 
 import type { AnyAttributeDefaultValue } from './types'
 
-/**
- * Input options of Any Attribute
- */
-export interface AnyAttributeOptions<
-  IS_REQUIRED extends RequiredOption = RequiredOption,
-  IS_HIDDEN extends boolean = boolean,
-  IS_KEY extends boolean = boolean,
-  SAVED_AS extends string | undefined = string | undefined,
-  DEFAULT extends AnyAttributeDefaultValue = AnyAttributeDefaultValue
-> extends AttributeOptions<IS_REQUIRED, IS_HIDDEN, IS_KEY, SAVED_AS> {
+// Note: May sound like a duplicate of AnyAttributeState but actually adds JSDocs
+
+export interface AnyAttributeOptions {
+  /**
+   * Tag attribute as required. Possible values are:
+   * - `"atLeastOnce"` _(default)_: Required in PUTs, optional in UPDATEs
+   * - `"never"`: Optional in PUTs and UPDATEs
+   * - `"always"`: Required in PUTs and UPDATEs
+   * - `"onlyOnce"`: Required in PUTs, denied in UPDATEs
+   */
+  required: RequiredOption
+  /**
+   * Hide attribute after fetch commands and formatting
+   */
+  hidden: boolean
+  /**
+   * Tag attribute as needed for Primary Key computing
+   */
+  key: boolean
+  /**
+   * Rename attribute before save commands
+   */
+  savedAs: string | undefined
   /**
    * Provide a default value for attribute, or tag attribute as having a computed default value
    */
-  default: DEFAULT
+  default: AnyAttributeDefaultValue
 }
 
-export const ANY_DEFAULT_OPTIONS: AnyAttributeOptions<
-  AtLeastOnce,
-  false,
-  false,
-  undefined,
-  undefined
-> = {
+export type AnyAttributeDefaultOptions = {
+  required: 'atLeastOnce'
+  hidden: false
+  key: false
+  savedAs: undefined
+  default: undefined
+}
+
+export const ANY_DEFAULT_OPTIONS: AnyAttributeDefaultOptions = {
   required: 'atLeastOnce',
   hidden: false,
   key: false,
