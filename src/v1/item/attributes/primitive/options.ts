48ae6bdc7f86e699a9c93affbb8e1ee1c33bf198
ThomasAribart <thomasa@theodo.fr>
2023-01-30 15:53:31 +0100

    rework primitive attributes to use named properties

--- src/v1/item/attributes/primitive/options.ts
+++ src/v1/item/attributes/primitive/options.ts
@@ -1,45 +1,61 @@
-import type { AttributeOptions } from '../shared/options'
-import type { RequiredOption, AtLeastOnce } from '../constants/requiredOptions'
+import type { RequiredOption } from '../constants/requiredOptions'
 
 import type {
   PrimitiveAttributeType,
-  PrimitiveAttributeEnumValues,
-  PrimitiveAttributeDefaultValue
+  PrimitiveAttributeDefaultValue,
+  PrimitiveAttributeEnumValues
 } from './types'
+
+// Note: May sound like a duplicate of AnyAttributeState but actually adds JSDocs
 
 /**
  * Input options of Primitive Attribute
  */
-export interface PrimitiveAttributeOptions<
-  TYPE extends PrimitiveAttributeType = PrimitiveAttributeType,
-  IS_REQUIRED extends RequiredOption = RequiredOption,
-  IS_HIDDEN extends boolean = boolean,
-  IS_KEY extends boolean = boolean,
-  SAVED_AS extends string | undefined = string | undefined,
-  ENUM extends PrimitiveAttributeEnumValues<TYPE> = PrimitiveAttributeEnumValues<TYPE>,
-  DEFAULT extends PrimitiveAttributeDefaultValue<TYPE> = PrimitiveAttributeDefaultValue<TYPE>
-> extends AttributeOptions<IS_REQUIRED, IS_HIDDEN, IS_KEY, SAVED_AS> {
-  // TODO: See if possible to use symbol
-  _enum: ENUM
+export type PrimitiveAttributeOptions<
+  TYPE extends PrimitiveAttributeType = PrimitiveAttributeType
+> = {
+  /**
+   * Tag attribute as required. Possible values are:
+   * - `"atLeastOnce"` _(default)_: Required in PUTs, optional in UPDATEs
+   * - `"never"`: Optional in PUTs and UPDATEs
+   * - `"always"`: Required in PUTs and UPDATEs
+   * - `"onlyOnce"`: Required in PUTs, denied in UPDATEs
+   */
+  required: RequiredOption
+  /**
+   * Hide attribute after fetch commands and formatting
+   */
+  hidden: boolean
+  /**
+   * Tag attribute as needed for Primary Key computing
+   */
+  key: boolean
+  /**
+   * Rename attribute before save commands
+   */
+  savedAs: string | undefined
+  // TODO: Use symbol
+  enum: PrimitiveAttributeEnumValues<TYPE>
   /**
    * Provide a default value for attribute, or tag attribute as having a computed default value
    */
-  default: DEFAULT
+  default: PrimitiveAttributeDefaultValue<TYPE>
 }
 
-export const PRIMITIVE_DEFAULT_OPTIONS: PrimitiveAttributeOptions<
-  PrimitiveAttributeType,
-  AtLeastOnce,
-  false,
-  false,
-  undefined,
-  undefined,
-  undefined
-> = {
+export type PrimitiveAttributeDefaultOptions = {
+  required: 'atLeastOnce'
+  hidden: false
+  key: false
+  savedAs: undefined
+  enum: undefined
+  default: undefined
+}
+
+export const PRIMITIVE_DEFAULT_OPTIONS: PrimitiveAttributeDefaultOptions = {
   required: 'atLeastOnce',
   hidden: false,
   key: false,
   savedAs: undefined,
-  _enum: undefined,
+  enum: undefined,
   default: undefined
 }
