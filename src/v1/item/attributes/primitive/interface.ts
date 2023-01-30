bf8b66a59b83d7c066f9c80d2569705a9bff4723
ThomasAribart <thomasa@theodo.fr>
2023-01-30 15:53:31 +0100

    rework primitive attributes to use named properties

--- src/v1/item/attributes/primitive/interface.ts
+++ src/v1/item/attributes/primitive/interface.ts
@@ -1,5 +1,6 @@
+import type { O } from 'ts-toolbelt'
+
 import type { RequiredOption, AtLeastOnce } from '../constants/requiredOptions'
-import type { _AttributeProperties, AttributeProperties } from '../shared/interface'
 import {
   $type,
   $resolved,
@@ -17,26 +18,36 @@ import type {
   PrimitiveAttributeEnumValues,
   PrimitiveAttributeDefaultValue
 } from './types'
+import { FreezeAttributeStateConstraint } from '../shared/freezeAttributeStateConstraint'
 
-// TODO: Define reqKey / optKey or partitionKey / sortKey shorthands ?
+interface _PrimitiveAttributeStateConstraint<
+  TYPE extends PrimitiveAttributeType = PrimitiveAttributeType
+> {
+  [$type]: TYPE
+  [$required]: RequiredOption
+  [$hidden]: boolean
+  [$key]: boolean
+  [$savedAs]: string | undefined
+  [$enum]: PrimitiveAttributeEnumValues<TYPE>
+  [$default]: PrimitiveAttributeDefaultValue<TYPE>
+}
+
 /**
  * Primitive attribute interface
  */
 export type _PrimitiveAttribute<
-  TYPE extends PrimitiveAttributeType = PrimitiveAttributeType,
-  IS_REQUIRED extends RequiredOption = RequiredOption,
-  IS_HIDDEN extends boolean = boolean,
-  IS_KEY extends boolean = boolean,
-  SAVED_AS extends string | undefined = string | undefined,
-  ENUM extends PrimitiveAttributeEnumValues<TYPE> = PrimitiveAttributeEnumValues<TYPE>,
-  DEFAULT extends PrimitiveAttributeDefaultValue<TYPE> = PrimitiveAttributeDefaultValue<TYPE>
-> = _AttributeProperties<IS_REQUIRED, IS_HIDDEN, IS_KEY, SAVED_AS> & {
-  [$type]: TYPE
-  [$resolved]?: ENUM extends ResolvePrimitiveAttributeType<TYPE>[]
-    ? ENUM[number]
-    : ResolvePrimitiveAttributeType<TYPE>
-  [$enum]: ENUM
-  [$default]: DEFAULT
+  STATE extends _PrimitiveAttributeStateConstraint = _PrimitiveAttributeStateConstraint
+> = {
+  [$type]: STATE[$type]
+  [$resolved]?: STATE[$enum] extends ResolvePrimitiveAttributeType<STATE[$type]>[]
+    ? STATE[$enum][number]
+    : ResolvePrimitiveAttributeType<STATE[$type]>
+  [$required]: STATE[$required]
+  [$hidden]: STATE[$hidden]
+  [$key]: STATE[$key]
+  [$savedAs]: STATE[$savedAs]
+  [$enum]: STATE[$enum]
+  [$default]: STATE[$default]
   /**
    * Tag attribute as required. Possible values are:
    * - `"atLeastOnce"` _(default)_: Required in PUTs, optional in UPDATEs
@@ -48,25 +59,25 @@ export type _PrimitiveAttribute<
    */
   required: <NEXT_IS_REQUIRED extends RequiredOption = AtLeastOnce>(
     nextRequired?: NEXT_IS_REQUIRED
-  ) => _PrimitiveAttribute<TYPE, NEXT_IS_REQUIRED, IS_HIDDEN, IS_KEY, SAVED_AS, ENUM, DEFAULT>
+  ) => _PrimitiveAttribute<O.Update<STATE, $required, NEXT_IS_REQUIRED>>
   /**
    * Shorthand for `required('never')`
    */
-  optional: () => _PrimitiveAttribute<TYPE, 'never', IS_HIDDEN, IS_KEY, SAVED_AS, ENUM, DEFAULT>
+  optional: () => _PrimitiveAttribute<O.Update<STATE, $required, 'never'>>
   /**
    * Hide attribute after fetch commands and formatting
    */
-  hidden: () => _PrimitiveAttribute<TYPE, IS_REQUIRED, true, IS_KEY, SAVED_AS, ENUM, DEFAULT>
+  hidden: () => _PrimitiveAttribute<O.Update<STATE, $hidden, true>>
   /**
    * Tag attribute as needed for Primary Key computing
    */
-  key: () => _PrimitiveAttribute<TYPE, IS_REQUIRED, IS_HIDDEN, true, SAVED_AS, ENUM, DEFAULT>
+  key: () => _PrimitiveAttribute<O.Update<STATE, $key, true>>
   /**
    * Rename attribute before save commands
    */
   savedAs: <NEXT_SAVED_AS extends string | undefined>(
     nextSavedAs: NEXT_SAVED_AS
-  ) => _PrimitiveAttribute<TYPE, IS_REQUIRED, IS_HIDDEN, IS_KEY, NEXT_SAVED_AS, ENUM, DEFAULT>
+  ) => _PrimitiveAttribute<O.Update<STATE, $savedAs, NEXT_SAVED_AS>>
   /**
    * Provide a finite list of possible values for attribute
    * (For typing reasons, enums are only available as attribute methods, not as input options)
@@ -75,53 +86,40 @@ export type _PrimitiveAttribute<
    * @example
    * string().enum('foo', 'bar')
    */
-  enum: <NEXT_ENUM extends ResolvePrimitiveAttributeType<TYPE>[]>(
+  enum: <NEXT_ENUM extends ResolvePrimitiveAttributeType<STATE[$type]>[]>(
     ...nextEnum: NEXT_ENUM
-  ) => _PrimitiveAttribute<TYPE, IS_REQUIRED, IS_HIDDEN, IS_KEY, SAVED_AS, NEXT_ENUM, DEFAULT>
+  ) => _PrimitiveAttribute<O.Update<STATE, $enum, NEXT_ENUM>>
   /**
    * Provide a default value for attribute, or tag attribute as having a computed default value
    *
    * @param nextDefaultValue `Attribute type`, `() => Attribute type`, `ComputedDefault`
    */
   default: <
-    NEXT_DEFAULT extends PrimitiveAttributeDefaultValue<TYPE> &
-      (ENUM extends ResolvePrimitiveAttributeType<TYPE>[]
-        ? ENUM[number] | (() => ENUM[number])
+    NEXT_DEFAULT extends PrimitiveAttributeDefaultValue<STATE[$type]> &
+      (STATE[$enum] extends ResolvePrimitiveAttributeType<STATE[$type]>[]
+        ? STATE[$enum][number] | (() => STATE[$enum][number])
         : unknown)
   >(
     nextDefaultValue: NEXT_DEFAULT
-  ) => _PrimitiveAttribute<TYPE, IS_REQUIRED, IS_HIDDEN, IS_KEY, SAVED_AS, ENUM, NEXT_DEFAULT>
+  ) => _PrimitiveAttribute<O.Update<STATE, $default, NEXT_DEFAULT>>
 }
+
+export type PrimitiveAttributeStateConstraint<
+  TYPE extends PrimitiveAttributeType = PrimitiveAttributeType
+> = FreezeAttributeStateConstraint<_PrimitiveAttributeStateConstraint<TYPE>>
 
 export type PrimitiveAttribute<
-  TYPE extends PrimitiveAttributeType = PrimitiveAttributeType,
-  IS_REQUIRED extends RequiredOption = RequiredOption,
-  IS_HIDDEN extends boolean = boolean,
-  IS_KEY extends boolean = boolean,
-  SAVED_AS extends string | undefined = string | undefined,
-  ENUM extends PrimitiveAttributeEnumValues<TYPE> = PrimitiveAttributeEnumValues<TYPE>,
-  DEFAULT extends PrimitiveAttributeDefaultValue<TYPE> = PrimitiveAttributeDefaultValue<TYPE>
-> = AttributeProperties<IS_REQUIRED, IS_HIDDEN, IS_KEY, SAVED_AS> & {
+  STATE extends PrimitiveAttributeStateConstraint = PrimitiveAttributeStateConstraint
+> = {
   path: string
-  type: TYPE
-  resolved?: ENUM extends ResolvePrimitiveAttributeType<TYPE>[]
-    ? ENUM[number]
-    : ResolvePrimitiveAttributeType<TYPE>
-  enum: ENUM
-  default: DEFAULT
+  type: STATE['type']
+  resolved?: STATE['enum'] extends ResolvePrimitiveAttributeType<STATE['type']>[]
+    ? STATE['enum'][number]
+    : ResolvePrimitiveAttributeType<STATE['type']>
+  required: STATE['required']
+  hidden: STATE['hidden']
+  key: STATE['key']
+  savedAs: STATE['savedAs']
+  enum: STATE['enum']
+  default: STATE['default']
 }
-
-export type FreezePrimitiveAttribute<
-  _PRIMITIVE_ATTRIBUTE extends _PrimitiveAttribute
-> = PrimitiveAttribute<
-  _PRIMITIVE_ATTRIBUTE[$type],
-  _PRIMITIVE_ATTRIBUTE[$required],
-  _PRIMITIVE_ATTRIBUTE[$hidden],
-  _PRIMITIVE_ATTRIBUTE[$key],
-  _PRIMITIVE_ATTRIBUTE[$savedAs],
-  Extract<_PRIMITIVE_ATTRIBUTE[$enum], PrimitiveAttributeEnumValues<_PRIMITIVE_ATTRIBUTE[$type]>>,
-  Extract<
-    _PRIMITIVE_ATTRIBUTE[$default],
-    PrimitiveAttributeDefaultValue<_PRIMITIVE_ATTRIBUTE[$type]>
-  >
->
