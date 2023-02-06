210e80bf7e13510ab293cc89911dbe2f516b0a78
ThomasAribart <thomasa@theodo.fr>
2023-02-06 17:38:35 +0100

    enable capacity & metrics options in putItems

--- src/v1/commands/putItem/putItemParams/putItemParams.ts
+++ src/v1/commands/putItem/putItemParams/putItemParams.ts
@@ -1,21 +1,26 @@
-import { PutCommandInput } from '@aws-sdk/lib-dynamodb'
+import type { PutCommandInput } from '@aws-sdk/lib-dynamodb'
 
-import { EntityV2, PutItemInput } from 'v1'
+import type { EntityV2, PutItemInput } from 'v1'
 import { parsePrimaryKey } from 'v1/commands/utils/parsePrimaryKey'
 import { renameSavedAsAttributes } from 'v1/commands/utils/renameSavedAsAttributes'
 
+import type { PutItemOptions } from '../options'
+
 import { parseEntityPutCommandInput } from './parsePutCommandInput'
+import { parsePutItemOptions } from './parsePutItemOptions'
 
 /**
  * Builds a PUT Item command input for a given Entity
  *
  * @param entity Entity
  * @param input Input
+ * @param putItemOptions PutItemOptions
  * @return PutCommandInput
  */
 export const putItemParams = <ENTITY extends EntityV2>(
   entity: ENTITY,
-  input: PutItemInput<ENTITY, false>
+  input: PutItemInput<ENTITY, false>,
+  putItemOptions: PutItemOptions = {}
 ): PutCommandInput => {
   const validInput = parseEntityPutCommandInput<ENTITY>(entity, input)
 
@@ -26,8 +31,11 @@ export const putItemParams = <ENTITY extends EntityV2>(
 
   const primaryKey = parsePrimaryKey(entity, keyInput ?? renamedInput)
 
+  const options = parsePutItemOptions(putItemOptions)
+
   return {
     TableName: entity.table.name,
-    Item: { ...renamedInput, ...primaryKey }
+    Item: { ...renamedInput, ...primaryKey },
+    ...options
   }
 }
