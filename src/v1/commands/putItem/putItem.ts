1df23c3e83ca34ae30e30a713764e3bb86abd3c1
ThomasAribart <thomasa@theodo.fr>
2023-02-06 18:19:16 +0100

    dynamically type the putItem response according to the provided returnValues option

--- src/v1/commands/putItem/putItem.ts
+++ src/v1/commands/putItem/putItem.ts
@@ -4,8 +4,29 @@ import { PutCommand, PutCommandOutput } from '@aws-sdk/lib-dynamodb'
 import type { EntityV2, PutItemInput, FormattedItem } from 'v1'
 import { parseSavedItem } from 'v1/commands/utils/parseSavedItem'
 
-import type { PutItemOptions } from './options'
+import type {
+  PutItemOptions,
+  ReturnValuesOption,
+  None,
+  UpdatedOld,
+  UpdatedNew,
+  AllOld,
+  AllNew
+} from './options'
 import { putItemParams } from './putItemParams'
+
+type ReturnedAttributes<
+  ENTITY extends EntityV2,
+  RETURN_VALUES extends ReturnValuesOption
+> = ReturnValuesOption extends RETURN_VALUES
+  ? undefined
+  : RETURN_VALUES extends None
+  ? undefined
+  : RETURN_VALUES extends UpdatedOld | UpdatedNew
+  ? Partial<FormattedItem<ENTITY>> | undefined
+  : RETURN_VALUES extends AllNew | AllOld
+  ? FormattedItem<ENTITY> | undefined
+  : never
 
 /**
  * Run a PUT Item command for a given Entity
@@ -15,12 +36,18 @@ import { putItemParams } from './putItemParams'
  * @param putItemOptions PutItemOptions
  * @return PutCommandOutput
  */
-export const putItem = async <ENTITY extends EntityV2>(
+export const putItem = async <
+  ENTITY extends EntityV2,
+  RETURN_VALUES extends ReturnValuesOption = ReturnValuesOption
+>(
   entity: ENTITY,
   putItemInput: PutItemInput<ENTITY>,
-  putItemOptions: PutItemOptions = {}
+  putItemOptions: PutItemOptions<RETURN_VALUES> = {}
 ): Promise<
-  O.Merge<Omit<PutCommandOutput, 'Attributes'>, { Attributes?: FormattedItem<ENTITY> | undefined }>
+  O.Merge<
+    Omit<PutCommandOutput, 'Attributes'>,
+    { Attributes?: ReturnedAttributes<ENTITY, RETURN_VALUES> }
+  >
 > => {
   const commandOutput = await entity.table.documentClient.send(
     new PutCommand(putItemParams<ENTITY>(entity, putItemInput, putItemOptions))
@@ -32,7 +59,13 @@ export const putItem = async <ENTITY extends EntityV2>(
     return restCommandOutput
   }
 
+  // TODO: Create parseSavedAttributes util that handles partial Items (for the moment, it will throw)
+  // (returned for UpdatedOld/UpdatedNew returnValues option)
+  // (Also: is the partial flat or deep ?)
   const formattedItem = parseSavedItem(entity, attributes)
 
-  return { Attributes: formattedItem, ...restCommandOutput }
+  return {
+    Attributes: formattedItem as ReturnedAttributes<ENTITY, RETURN_VALUES>,
+    ...restCommandOutput
+  }
 }
