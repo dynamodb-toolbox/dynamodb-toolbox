9a3bfd47aec8fff046cb7587a98508b7efb382d7
ThomasAribart <thomasa@theodo.fr>
2023-02-06 17:38:35 +0100

    enable capacity & metrics options in putItems

--- src/v1/commands/putItem/putItem.ts
+++ src/v1/commands/putItem/putItem.ts
@@ -1,9 +1,10 @@
 import type { O } from 'ts-toolbelt'
 import { PutCommand, PutCommandOutput } from '@aws-sdk/lib-dynamodb'
 
-import { EntityV2, PutItemInput, FormattedItem } from 'v1'
+import type { EntityV2, PutItemInput, FormattedItem } from 'v1'
 import { parseSavedItem } from 'v1/commands/utils/parseSavedItem'
 
+import type { PutItemOptions } from './options'
 import { putItemParams } from './putItemParams'
 
 /**
@@ -11,16 +12,18 @@ import { putItemParams } from './putItemParams'
  *
  * @param entity Entity
  * @param putItemInput PutItemInput
+ * @param putItemOptions PutItemOptions
  * @return PutCommandOutput
  */
 export const putItem = async <ENTITY extends EntityV2>(
   entity: ENTITY,
-  putItemInput: PutItemInput<ENTITY>
+  putItemInput: PutItemInput<ENTITY>,
+  putItemOptions: PutItemOptions = {}
 ): Promise<
   O.Merge<Omit<PutCommandOutput, 'Attributes'>, { Attributes?: FormattedItem<ENTITY> | undefined }>
 > => {
   const commandOutput = await entity.table.documentClient.send(
-    new PutCommand(putItemParams<ENTITY>(entity, putItemInput))
+    new PutCommand(putItemParams<ENTITY>(entity, putItemInput, putItemOptions))
   )
 
   const { Attributes: attributes, ...restCommandOutput } = commandOutput
