87fbd5ab71e8d6ccbf0271fea30f7b024a469976
ThomasAribart <thomasa@theodo.fr>
2023-02-06 18:02:17 +0100

    validate returnValues and extra options as well

--- src/v1/commands/putItem/options.ts
+++ src/v1/commands/putItem/options.ts
@@ -11,7 +11,22 @@ export type MetricsOption = None | Size
 
 export const metricsOptionsSet = new Set<MetricsOption>(['NONE', 'SIZE'])
 
+export type AllOld = 'ALL_OLD'
+export type UpdatedOld = 'UPDATED_OLD'
+export type AllNew = 'ALL_NEW'
+export type UpdatedNew = 'UPDATED_NEW'
+export type ReturnValuesOption = None | AllOld | UpdatedOld | AllNew | UpdatedNew
+
+export const returnValuesOptionsSet = new Set<ReturnValuesOption>([
+  'NONE',
+  'ALL_OLD',
+  'UPDATED_OLD',
+  'ALL_NEW',
+  'UPDATED_NEW'
+])
+
 export interface PutItemOptions {
   capacity?: CapacityOption
   metrics?: MetricsOption
+  returnValues?: ReturnValuesOption
 }
