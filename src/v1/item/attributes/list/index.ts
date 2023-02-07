b7c29626e9d88485cda3e7382434ce4d235a8e6d
ThomasAribart <thomasa@theodo.fr>
2023-02-07 18:32:34 +0100

    migrate list attribute errors to DynamoDBToolboxError class

--- src/v1/item/attributes/list/index.ts
+++ src/v1/item/attributes/list/index.ts
@@ -1,10 +1,3 @@
 export { list } from './typer'
 export type { $ListAttribute, ListAttribute } from './interface'
-export {
-  FreezeListAttribute,
-  HiddenListAttributeElementsError,
-  SavedAsListAttributeElementsError,
-  OptionalListAttributeElementsError,
-  DefaultedListAttributeElementsError,
-  freezeListAttribute
-} from './freeze'
+export { FreezeListAttribute, freezeListAttribute } from './freeze'
