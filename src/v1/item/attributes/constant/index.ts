9b3216165e5e8898affa9d68f9a2a7ded337f5cb
ThomasAribart <thomasa@theodo.fr>
2023-02-07 12:11:00 +0100

    move some already existing errors to DynamoDBToolboxError

--- src/v1/item/attributes/constant/index.ts
+++ src/v1/item/attributes/constant/index.ts
@@ -1,8 +1,4 @@
 export { constant } from './typer'
 export type { $ConstantAttribute, ConstantAttribute } from './interface'
-export {
-  FreezeConstantAttribute,
-  freezeConstantAttribute,
-  InvalidDefaultValueError
-} from './freeze'
+export { FreezeConstantAttribute, freezeConstantAttribute } from './freeze'
 export type { $ResolveConstantAttribute, ResolveConstantAttribute } from './resolve'
