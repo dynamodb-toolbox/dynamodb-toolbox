925e315be9c09e7d9fb1e641b80aac19b978f71c
ThomasAribart <thomasa@theodo.fr>
2023-02-08 01:31:44 +0100

    split error blueprints by folder

--- src/v1/item/attributes/constant/errors.ts
+++ src/v1/item/attributes/constant/errors.ts
@@ -2,8 +2,10 @@ import type { ErrorBlueprint } from 'v1/errors/blueprint'
 
 import type { ResolvedAttribute } from '../types'
 
-export type InvalidConstantAttributeDefaultValueErrorBlueprint = ErrorBlueprint<{
+type InvalidConstantAttributeDefaultValueErrorBlueprint = ErrorBlueprint<{
   code: 'invalidConstantAttributeDefaultValue'
   hasPath: true
   payload: { expectedValue: ResolvedAttribute; defaultValue: unknown }
 }>
+
+export type ConstantAttributeErrorBlueprints = InvalidConstantAttributeDefaultValueErrorBlueprint
