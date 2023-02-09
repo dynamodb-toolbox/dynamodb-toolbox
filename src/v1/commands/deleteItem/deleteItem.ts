ba3440749d955c43496e6ffc777096f4c5e3e01c
ThomasAribart <thomasa@theodo.fr>
2023-02-09 10:40:36 +0100

    start implementing deleteItem options: Factorize with putItem & getItem options validation

--- src/v1/commands/deleteItem/deleteItem.ts
+++ src/v1/commands/deleteItem/deleteItem.ts
@@ -5,22 +5,25 @@ import { EntityV2, FormattedItem, KeyInput } from 'v1'
 import { parseSavedItem } from 'v1/commands/utils/parseSavedItem'
 
 import { deleteItemParams } from './deleteItemParams'
+import type { DeleteItemOptions } from './options'
 
 /**
  * Run a Delete Item command for a given Entity
  *
  * @param entity Entity
  * @param keyInput KeyInput
+ * @param deleteItemOptions DeleteItemOptions
  * @return DeleteItemCommandOutput
  */
 export const deleteItem = async <ENTITY extends EntityV2>(
   entity: ENTITY,
-  keyInput: KeyInput<ENTITY>
+  keyInput: KeyInput<ENTITY>,
+  deleteItemOptions: DeleteItemOptions = {}
 ): Promise<
   O.Merge<Omit<DeleteCommandOutput, 'Attributes'>, { Item?: FormattedItem<ENTITY> | undefined }>
 > => {
   const commandOutput = await entity.table.documentClient.send(
-    new DeleteCommand(deleteItemParams<ENTITY>(entity, keyInput))
+    new DeleteCommand(deleteItemParams<ENTITY>(entity, keyInput, deleteItemOptions))
   )
 
   const { Attributes: attributes, ...restCommandOutput } = commandOutput
