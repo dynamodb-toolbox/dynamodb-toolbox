79d998252cd7275555a03bc3636451726f9a42f3
ThomasAribart <thomasa@theodo.fr>
2023-02-09 10:40:36 +0100

    start implementing deleteItem options: Factorize with putItem & getItem options validation

---
+++ src/v1/commands/deleteItem/options.ts
@@ -0,0 +1,7 @@
+import { CapacityOption } from 'v1/commands/constants/options/capacity'
+
+export interface DeleteItemOptions {
+  capacity?: CapacityOption
+  // metrics?: MetricsOption
+  // returnValues?: RETURN_VALUES
+}
