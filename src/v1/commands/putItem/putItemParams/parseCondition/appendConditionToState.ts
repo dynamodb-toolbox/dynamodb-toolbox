7b641aa05898077508cda90ca707e465ac27ada3
ThomasAribart <thomasa@theodo.fr>
2023-03-20 16:57:57 +0100

    implement not condition

--- src/v1/commands/putItem/putItemParams/parseCondition/appendConditionToState.ts
+++ src/v1/commands/putItem/putItemParams/parseCondition/appendConditionToState.ts
@@ -4,6 +4,7 @@ import { ParsingState } from './types'
 import { isComparisonCondition, appendComparisonConditionToState } from './comparison'
 import { isSingleArgFnCondition, appendSingleArgFnConditionToState } from './singleArgFn'
 import { isBetweenCondition, appendBetweenConditionToState } from './between'
+import { isNotCondition, appendNotConditionToState } from './not'
 
 export const appendConditionToState = (state: ParsingState, condition: Condition): ParsingState => {
   if (isComparisonCondition(condition)) {
@@ -16,6 +17,10 @@ export const appendConditionToState = (state: ParsingState, condition: Condition
 
   if (isBetweenCondition(condition)) {
     return appendBetweenConditionToState(state, condition)
+  }
+
+  if (isNotCondition(condition)) {
+    return appendNotConditionToState(state, condition)
   }
 
   return state
