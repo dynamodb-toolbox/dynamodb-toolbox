776d0ee77df4c4966a41306fedb627fd63347cca
ThomasAribart <thomasa@theodo.fr>
2023-04-17 14:33:06 +0200

    add entityNameAttribute option in Table

--- src/v1/table/class.ts
+++ src/v1/table/class.ts
@@ -2,11 +2,16 @@ import type { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb'
 
 import { Key, NarrowKey } from './types'
 
-export class TableV2<PARTITION_KEY extends Key = Key, SORT_KEY extends Key = Key> {
+export class TableV2<
+  PARTITION_KEY extends Key = Key,
+  SORT_KEY extends Key = Key,
+  ENTITY_NAME_ATTRIBUTE_SAVED_AS extends string = Key extends PARTITION_KEY ? string : '_et'
+> {
   public documentClient: DynamoDBDocumentClient
   public name: string
   public partitionKey: PARTITION_KEY
   public sortKey?: SORT_KEY
+  public entityNameAttributeSavedAs: ENTITY_NAME_ATTRIBUTE_SAVED_AS
 
   /**
    * Define a Table
@@ -16,17 +21,20 @@ export class TableV2<PARTITION_KEY extends Key = Key, SORT_KEY extends Key = Key
    * @param name string
    * @param partitionKey Partition key
    * @param sortKey _(optional)_ Sort key
+   * @param entityNameAttributeSavedAs _(optional)_ Entity name attribute savedAs (defaults to `'_et'`)
    */
   constructor({
     documentClient,
     name,
     partitionKey,
-    sortKey
+    sortKey,
+    entityNameAttributeSavedAs = '_et' as ENTITY_NAME_ATTRIBUTE_SAVED_AS
   }: {
     documentClient: DynamoDBDocumentClient
     name: string
     partitionKey: NarrowKey<PARTITION_KEY>
     sortKey?: NarrowKey<SORT_KEY>
+    entityNameAttributeSavedAs?: ENTITY_NAME_ATTRIBUTE_SAVED_AS
   }) {
     this.documentClient = documentClient
     this.name = name
@@ -34,5 +42,6 @@ export class TableV2<PARTITION_KEY extends Key = Key, SORT_KEY extends Key = Key
     if (sortKey) {
       this.sortKey = sortKey
     }
+    this.entityNameAttributeSavedAs = entityNameAttributeSavedAs
   }
 }
