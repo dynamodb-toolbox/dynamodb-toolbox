806e5f4e3011d3afe01f2ad8977ea5898a8259c5
ThomasAribart <thomasa@theodo.fr>
2023-04-17 17:06:28 +0200

    fix bug in conditions parsing

--- src/v1/commands/utils/parseCondition/parseCondition.ts
+++ src/v1/commands/utils/parseCondition/parseCondition.ts
@@ -1,4 +1,4 @@
-import { NativeAttributeValue } from '@aws-sdk/util-dynamodb'
+import type { NativeAttributeValue } from '@aws-sdk/util-dynamodb'
 
 import type { Item } from 'v1/item'
 import type { Condition } from 'v1/commands/condition/types'
@@ -9,9 +9,9 @@ export const parseCondition = <ITEM extends Item, CONDITION extends Condition<IT
   item: ITEM,
   condition: CONDITION
 ): {
-  ConditionExpression?: string
-  ExpressionAttributeNames?: Record<string, string>
-  ExpressionAttributeValues?: Record<string, NativeAttributeValue>
+  ConditionExpression: string
+  ExpressionAttributeNames: Record<string, string>
+  ExpressionAttributeValues: Record<string, NativeAttributeValue>
 } => {
   const conditionParser = new ConditionParser(item)
   conditionParser.parseCondition(condition)
