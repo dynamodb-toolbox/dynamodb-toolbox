6dac6604e7a7b1e95fecb4848d6f0f66d30a48c2
ThomasAribart <thomasa@theodo.fr>
2023-04-20 14:05:26 +0200

    support anyOf attributes in formatSavedItem

--- src/v1/commands/putItem/putItemParams/parsePutCommandInput/set.ts
+++ src/v1/commands/putItem/putItemParams/parsePutCommandInput/set.ts
@@ -1,4 +1,4 @@
-import type { SetAttribute, PossiblyUndefinedResolvedAttribute } from 'v1'
+import type { SetAttribute, PossiblyUndefinedResolvedAttribute } from 'v1/item'
 import { isSet } from 'v1/utils/validation'
 import { DynamoDBToolboxError } from 'v1/errors'
 
--- src/v1/commands/utils/formatSavedItem/attribute.ts
+++ src/v1/commands/utils/formatSavedItem/attribute.ts
@@ -1,24 +1,42 @@
 import cloneDeep from 'lodash.clonedeep'
 
-import { Attribute, ResolvedAttribute } from 'v1/item'
+import type { Attribute, RequiredOption, PossiblyUndefinedResolvedAttribute } from 'v1/item'
 
+import { parseSavedPrimitiveAttribute } from './primitive'
+import { parseSavedSetAttribute } from './set'
 import { parseSavedListAttribute } from './list'
 import { parseSavedMapAttribute } from './map'
 import { parseSavedAnyOfAttribute } from './anyOf'
 import { parseSavedRecordAttribute } from './record'
 
+export const requiringOptions = new Set<RequiredOption>(['always', 'onlyOnce', 'atLeastOnce'])
+
+export const isRequired = (attribute: Attribute): boolean =>
+  requiringOptions.has(attribute.required)
+
 export const parseSavedAttribute = (
   attribute: Attribute,
-  value: ResolvedAttribute
-): ResolvedAttribute => {
+  value: PossiblyUndefinedResolvedAttribute
+): PossiblyUndefinedResolvedAttribute => {
+  if (value === undefined) {
+    if (isRequired(attribute)) {
+      // TODO
+      throw new Error()
+    } else {
+      return undefined
+    }
+  }
+
   switch (attribute.type) {
     case 'any':
+      return cloneDeep(value)
     case 'string':
     case 'binary':
     case 'boolean':
     case 'number':
+      return parseSavedPrimitiveAttribute(attribute, value)
     case 'set':
-      return cloneDeep(value)
+      return parseSavedSetAttribute(attribute, value)
     case 'list':
       return parseSavedListAttribute(attribute, value)
     case 'map':
