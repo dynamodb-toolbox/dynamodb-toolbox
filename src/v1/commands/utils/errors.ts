60e597c0482e267f91be5fefaa8fd780de072ff9
ThomasAribart <thomasa@theodo.fr>
2023-04-21 11:34:47 +0200

    use DynamoDBToolboxError in formatSavedItem

--- src/v1/commands/utils/errors.ts
+++ src/v1/commands/utils/errors.ts
@@ -1,6 +1,8 @@
 import type { ParseConditionErrorBlueprints } from './parseCondition/errors'
 import type { ParseKeyInputErrorBlueprints } from './parseKeyInput/errors'
+import type { FormatSavedItemErrorBlueprints } from './formatSavedItem/errors'
 
 export type CommandUtilsErrorBlueprints =
   | ParseConditionErrorBlueprints
   | ParseKeyInputErrorBlueprints
+  | FormatSavedItemErrorBlueprints
--- src/v1/commands/utils/formatSavedItem/list.ts
+++ src/v1/commands/utils/formatSavedItem/list.ts
@@ -1,16 +1,23 @@
 import type { PossiblyUndefinedResolvedAttribute, ListAttribute } from 'v1/item'
 import { isArray } from 'v1/utils/validation'
+import { DynamoDBToolboxError } from 'v1/errors'
 
 import { parseSavedAttribute } from './attribute'
 
 export const parseSavedListAttribute = (
   listAttribute: ListAttribute,
-  input: PossiblyUndefinedResolvedAttribute
+  value: PossiblyUndefinedResolvedAttribute
 ): PossiblyUndefinedResolvedAttribute => {
-  if (!isArray(input)) {
-    // TODO
-    throw new Error()
+  if (!isArray(value)) {
+    throw new DynamoDBToolboxError('commands.formatSavedItem.invalidSavedAttribute', {
+      message: `Invalid attribute in saved item: ${listAttribute.path}. Should be a ${listAttribute.type}`,
+      path: listAttribute.path,
+      payload: {
+        received: value,
+        expected: listAttribute.type
+      }
+    })
   }
 
-  return input.map(elementInput => parseSavedAttribute(listAttribute.elements, elementInput))
+  return value.map(elementInput => parseSavedAttribute(listAttribute.elements, elementInput))
 }
