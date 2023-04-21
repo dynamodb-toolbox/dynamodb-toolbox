76a147a7c529e731be0f8fe2b18fa686a6bc4a10
ThomasAribart <thomasa@theodo.fr>
2023-04-21 19:13:42 +0200

    implement last missing errors

--- src/v1/commands/utils/errors.ts
+++ src/v1/commands/utils/errors.ts
@@ -1,8 +1,10 @@
 import type { ParseConditionErrorBlueprints } from './parseCondition/errors'
 import type { ParseKeyInputErrorBlueprints } from './parseKeyInput/errors'
 import type { FormatSavedItemErrorBlueprints } from './formatSavedItem/errors'
+import type { ParsePrimaryKeyErrorBlueprints } from './parsePrimaryKey/errors'
 
 export type CommandUtilsErrorBlueprints =
   | ParseConditionErrorBlueprints
   | ParseKeyInputErrorBlueprints
   | FormatSavedItemErrorBlueprints
+  | ParsePrimaryKeyErrorBlueprints
