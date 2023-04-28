964bf88e24595a01e9191230a3472882b76164a4
ThomasAribart <thomasa@theodo.fr>
2023-04-28 11:38:45 +0200

    use new util in parseKeyInput

--- src/v1/commands/utils/errors.ts
+++ src/v1/commands/utils/errors.ts
@@ -1,10 +1,8 @@
 import type { ParseConditionErrorBlueprints } from './parseCondition/errors'
-import type { ParseKeyInputErrorBlueprints } from './parseKeyInput/errors'
 import type { FormatSavedItemErrorBlueprints } from './formatSavedItem/errors'
 import type { ParsePrimaryKeyErrorBlueprints } from './parsePrimaryKey/errors'
 
 export type CommandUtilsErrorBlueprints =
   | ParseConditionErrorBlueprints
-  | ParseKeyInputErrorBlueprints
   | FormatSavedItemErrorBlueprints
   | ParsePrimaryKeyErrorBlueprints
