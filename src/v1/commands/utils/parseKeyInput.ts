007474fef1de11f1443b90e4ce7bd5a90efb3787
ThomasAribart <thomasa@theodo.fr>
2023-04-28 11:38:45 +0200

    use new util in parseKeyInput

--- src/v1/commands/utils/parseKeyInput.ts
+++ src/v1/commands/utils/parseKeyInput.ts
@@ -1,17 +1,22 @@
 import type { EntityV2, KeyInput } from 'v1/entity'
-import type { PossiblyUndefinedResolvedItem } from 'v1/item'
+import type { PossiblyUndefinedResolvedItem, RequiredOption } from 'v1/item'
 import { cloneInputAndAddDefaults } from 'v1/validation/cloneInputAndAddDefaults'
 
-import { parseItemKeyInput } from './item'
+import { parseItemClonedInput } from 'v1/validation/parseClonedInput'
 
 type EntityKeyInputParser = <ENTITY extends EntityV2>(
   entity: ENTITY,
   input: PossiblyUndefinedResolvedItem
 ) => KeyInput<ENTITY>
 
+const requiringOptions = new Set<RequiredOption>(['always'])
+
 export const parseEntityKeyInput: EntityKeyInputParser = (entity, input) => {
-  // TODO: Run computeDefaults in single item commands
+  // Note: We do not provide defaults computer for now
   const clonedInputWithDefaults = cloneInputAndAddDefaults(entity.item, input)
 
-  return parseItemKeyInput(entity.item, clonedInputWithDefaults) as any
+  return parseItemClonedInput(entity.item, clonedInputWithDefaults, {
+    requiringOptions,
+    filters: { key: true }
+  }) as any
 }
