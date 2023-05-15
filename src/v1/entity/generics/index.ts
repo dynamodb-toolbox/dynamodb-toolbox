65c7a137f0737823ed93308c5d4a2563938472b4
ThomasAribart <thomasa@theodo.fr>
2023-05-16 01:04:26 +0200

    type getItem response Item according to projected attributes

--- src/v1/entity/generics/index.ts
+++ src/v1/entity/generics/index.ts
@@ -10,5 +10,5 @@ export type { PutItemInput, AttributePutItemInput } from './PutItemInput'
 export type { UpdateItemInput } from './UpdateItemInput'
 export type { UpdateItem } from './UpdateItem'
 
-export type { FormattedItem } from './FormattedItem'
+export type { FormattedItem, FormattedAttribute } from './FormattedItem'
 export type { SavedItem } from './SavedItem'
