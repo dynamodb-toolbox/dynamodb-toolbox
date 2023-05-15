b79b657ae108d0b2c4c9c925b8643439ab7d72d5
ThomasAribart <thomasa@theodo.fr>
2023-05-16 01:04:26 +0200

    type getItem response Item according to projected attributes

---
+++ src/v1/entity/generics/FormattedItem/record.ts
@@ -0,0 +1,31 @@
+import type { RecordAttribute, ResolvePrimitiveAttribute } from 'v1/item'
+import type { FormattedAttribute } from './attribute'
+import type { MatchKeys } from './utils'
+
+export type FormattedRecordAttribute<
+  RECORD_ATTRIBUTE extends RecordAttribute,
+  FILTERED_ATTRIBUTES extends string,
+  MATCHING_KEYS extends string = MatchKeys<
+    ResolvePrimitiveAttribute<RECORD_ATTRIBUTE['keys']>,
+    '.',
+    FILTERED_ATTRIBUTES
+  >
+> =
+  // Possible in case of anyOf subSchema
+  [MATCHING_KEYS] extends [never]
+    ? never
+    : Record<
+        MATCHING_KEYS,
+        FormattedAttribute<
+          RECORD_ATTRIBUTE['elements'],
+          MATCHING_KEYS extends infer FILTERED_KEY
+            ? FILTERED_KEY extends string
+              ? `.${FILTERED_KEY}` extends FILTERED_ATTRIBUTES
+                ? string
+                : FILTERED_ATTRIBUTES extends `.${FILTERED_KEY}${infer CHILDREN_FILTERED_ATTRIBUTES}`
+                ? CHILDREN_FILTERED_ATTRIBUTES
+                : never
+              : never
+            : never
+        >
+      >
