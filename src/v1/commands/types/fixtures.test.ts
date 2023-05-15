a2b5c07ca9e1b242ab353901cdc3f5a77537fc94
ThomasAribart <thomasa@theodo.fr>
2023-05-15 15:32:02 +0200

    split condition file in paths & condition

---
+++ src/v1/commands/types/fixtures.test.ts
@@ -0,0 +1,23 @@
+import { item, any, string, number, boolean, binary, set, list, map, record, anyOf } from 'v1/item'
+
+export const myItem = item({
+  parentId: string().key().savedAs('pk'),
+  childId: string().key().savedAs('sk'),
+  any: any(),
+  const: string().const('const'),
+  num: number(),
+  bool: boolean(),
+  bin: binary(),
+  stringSet: set(string()),
+  stringList: list(string()),
+  mapList: list(map({ num: number() })),
+  map: map({
+    num: number(),
+    stringList: list(string()),
+    map: map({
+      num: number()
+    })
+  }),
+  record: record(string().enum('foo', 'bar'), map({ num: number() })),
+  union: anyOf([map({ str: string() }), map({ num: number() })])
+})
