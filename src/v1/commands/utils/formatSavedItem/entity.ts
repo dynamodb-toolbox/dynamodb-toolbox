f56db118a1f85a59349a0564ee9150f705259291
ThomasAribart <thomasa@theodo.fr>
2023-05-15 19:18:06 +0200

    support projection expressions in formatSavedItem

--- src/v1/commands/utils/formatSavedItem/entity.ts
+++ src/v1/commands/utils/formatSavedItem/entity.ts
@@ -1,12 +1,23 @@
 import { EntityV2, FormattedItem } from 'v1/entity'
 import { ResolvedItem, PossiblyUndefinedResolvedItem } from 'v1/item'
+import { AnyAttributePath } from 'v1/commands/types/paths'
 
 import { parseSavedAttribute } from './attribute'
+import { matchProjection } from './utils'
 
-export const formatSavedItem = <ENTITY extends EntityV2>(
+type FormatSavedItemOptions<ENTITY extends EntityV2> = {
+  projectedAttributes?: AnyAttributePath<ENTITY['item']>[]
+}
+
+export const formatSavedItem = <
+  ENTITY extends EntityV2,
+  OPTIONS extends FormatSavedItemOptions<ENTITY>
+>(
   entity: ENTITY,
-  savedItem: ResolvedItem
+  savedItem: ResolvedItem,
+  formatSavedItemOptions: OPTIONS = {} as OPTIONS
 ): FormattedItem<ENTITY> => {
+  const { projectedAttributes } = formatSavedItemOptions
   const formattedItem: PossiblyUndefinedResolvedItem = {}
 
   const item = entity.item
@@ -16,9 +27,21 @@ export const formatSavedItem = <ENTITY extends EntityV2>(
       return
     }
 
+    const { isProjected, childrenAttributes } = matchProjection(
+      new RegExp('^' + attributeName),
+      projectedAttributes
+    )
+
+    if (!isProjected) {
+      return
+    }
+
     const attributeSavedAs = attribute.savedAs ?? attributeName
 
-    const formattedAttribute = parseSavedAttribute(attribute, savedItem[attributeSavedAs])
+    const formattedAttribute = parseSavedAttribute(attribute, savedItem[attributeSavedAs], {
+      projectedAttributes: childrenAttributes
+    })
+
     if (formattedAttribute !== undefined) {
       formattedItem[attributeName] = formattedAttribute
     }
