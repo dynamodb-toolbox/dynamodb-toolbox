ee8e5205de49d8c4de98101863adfa81f126abe7
ThomasAribart <thomasa@theodo.fr>
2023-05-26 11:15:08 +0200

    export attribute and attr short hands

--- src/v1/item/attributes/index.ts
+++ src/v1/item/attributes/index.ts
@@ -9,3 +9,36 @@ export * from './anyOf'
 export * from './constants'
 export * from './types'
 export { freezeAttribute } from './freeze'
+
+import { any } from './any'
+import { binary, boolean, number, string } from './primitive'
+import { set } from './set'
+import { list } from './list'
+import { map } from './map'
+import { record } from './record'
+import { anyOf } from './anyOf'
+
+export const attribute: {
+  any: typeof any
+  binary: typeof binary
+  boolean: typeof boolean
+  number: typeof number
+  string: typeof string
+  set: typeof set
+  list: typeof list
+  map: typeof map
+  record: typeof record
+  anyOf: typeof anyOf
+} = {
+  any,
+  binary,
+  boolean,
+  number,
+  string,
+  set,
+  list,
+  map,
+  record,
+  anyOf
+}
+export const attr = attribute
