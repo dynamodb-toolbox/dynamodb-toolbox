f97caf436c27a5dffa4283ca5a80cde85b759563
ThomasAribart <thomasa@theodo.fr>
2023-06-09 11:55:46 +0200

    remove playground code

--- src/v1/playground/commands/getItem.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { GetItemCommand } from 'v1/commands'
-
-import { UserEntity } from '../entity'
-
-const test = async () => {
-  const test = await UserEntity.build(GetItemCommand).key({ userId: 'foo', age: 41 }).send()
-}
