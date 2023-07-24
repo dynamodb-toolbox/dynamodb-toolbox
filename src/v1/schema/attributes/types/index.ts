ca7a7d4b1cdaec62b90fd40d229ed986e2850437
Thomas Aribart <thomas.aribart.ext@axaclimate.com>
2023-07-24 15:24:06 +0200

    simplify PossiblyUndefinedResolvedAttribute and Item definitions

--- src/v1/schema/attributes/types/index.ts
+++ src/v1/schema/attributes/types/index.ts
@@ -1,17 +1,2 @@
-export type { Narrow } from './narrow'
-export type {
-  $Attribute,
-  Attribute,
-  $MapAttributeAttributes,
-  MapAttributeAttributes,
-  ResolvedListAttribute,
-  ResolvedSetAttribute,
-  ResolvedMapAttribute,
-  ResolvedAttribute,
-  ResolvedItem,
-  PossiblyUndefinedResolvedListAttribute,
-  PossiblyUndefinedResolvedSetAttribute,
-  PossiblyUndefinedResolvedMapAttribute,
-  PossiblyUndefinedResolvedAttribute,
-  PossiblyUndefinedResolvedItem
-} from './attribute'
+export * from './narrow'
+export * from './attribute'
