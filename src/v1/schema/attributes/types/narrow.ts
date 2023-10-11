d00e9db6ca40a5ceed3aed35589ad9656a09c00f
Thomas Aribart <thomas.aribart.ext@axaclimate.com>
2023-10-11 10:29:46 +0200

    enable combining schemas with the .and method

--- src/v1/schema/attributes/types/narrow.ts
+++ src/v1/schema/attributes/types/narrow.ts
@@ -1,13 +1,25 @@
-import { $MapAttributeAttributes, $Attribute } from './attribute'
+import { $MapAttributeAttributes, $Attribute, MapAttributeAttributes, Attribute } from './attribute'
 
 /**
  * Utility type to narrow the inferred attributes of a map or item
  *
- * @param AttributeInput MapAttributeAttributes | Attribute
+ * @param $ATTRIBUTE $MapAttributeAttributes | $Attribute
+ * @return $MapAttributeAttributes | $Attribute
+ */
+export type $Narrow<$ATTRIBUTE extends $MapAttributeAttributes | $Attribute> = {
+  [PROPERTY in keyof $ATTRIBUTE]: $ATTRIBUTE[PROPERTY] extends $MapAttributeAttributes | $Attribute
+    ? $Narrow<$ATTRIBUTE[PROPERTY]>
+    : $ATTRIBUTE[PROPERTY]
+}
+
+/**
+ * Utility type to narrow the inferred attributes of a map or item
+ *
+ * @param ATTRIBUTE MapAttributeAttributes | Attribute
  * @return MapAttributeAttributes | Attribute
  */
-export type Narrow<$ATTRIBUTE extends $MapAttributeAttributes | $Attribute> = {
-  [PROPERTY in keyof $ATTRIBUTE]: $ATTRIBUTE[PROPERTY] extends $MapAttributeAttributes | $Attribute
-    ? Narrow<$ATTRIBUTE[PROPERTY]>
-    : $ATTRIBUTE[PROPERTY]
+export type Narrow<ATTRIBUTE extends MapAttributeAttributes | Attribute> = {
+  [PROPERTY in keyof ATTRIBUTE]: ATTRIBUTE[PROPERTY] extends MapAttributeAttributes | Attribute
+    ? Narrow<ATTRIBUTE[PROPERTY]>
+    : ATTRIBUTE[PROPERTY]
 }
