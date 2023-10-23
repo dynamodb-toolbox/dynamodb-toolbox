c3b8ea814ab8d69bb7b46c0ef1d58f4eb448cdbf
Thomas Aribart <thomas.aribart.ext@axaclimate.com>
2023-10-23 16:52:45 +0200

    enable key defaults

--- src/v1/validation/cloneInputAndAddDefaults/types.ts
+++ src/v1/validation/cloneInputAndAddDefaults/types.ts
@@ -43,4 +43,4 @@ export type AnyOfAttributeClonedInputsWithDefaults<EXTENSION extends Extension =
   clonedInputsWithDefaults: AttributeValue<EXTENSION>[]
 }
 
-export type CommandName = 'put' | 'update'
+export type CommandName = 'key' | 'put' | 'update'
