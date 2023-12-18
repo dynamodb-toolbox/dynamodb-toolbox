3eb21b4778c2b3db0a5c2a4948c6fe7b83b09418
Guillaume Duboc <guillaumed@theodo.fr>
2023-12-18 18:59:17 +0100

    feat: add BatchPutItemRequest

---
+++ src/v1/operations/batch/putItem/BatchPutItemParams.unit.test.ts
@@ -0,0 +1,53 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb'
+
+import { DynamoDBToolboxError, EntityV2, TableV2, schema, string } from 'v1'
+import { BatchPutItemRequest } from './operation'
+
+const dynamoDbClient = new DynamoDBClient({})
+
+const documentClient = DynamoDBDocumentClient.from(dynamoDbClient)
+
+const TestTable = new TableV2({
+  name: 'test-table',
+  partitionKey: {
+    type: 'string',
+    name: 'pk'
+  },
+  sortKey: {
+    type: 'string',
+    name: 'sk'
+  },
+  documentClient
+})
+
+const TestEntity = new EntityV2({
+  name: 'TestEntity',
+  schema: schema({
+    email: string().key().savedAs('pk'),
+    sort: string().key().savedAs('sk'),
+    test_string: string()
+  }),
+  table: TestTable
+})
+
+describe('BatchPutItem', () => {
+  it('returns the result in the correct format', async () => {
+    const { Item } = TestEntity.build(BatchPutItemRequest)
+      .item({ email: 'test-pk', sort: 'test-sk', test_string: 'test string' })
+      .params()
+
+    expect(Item).toMatchObject({
+      pk: 'test-pk',
+      sk: 'test-sk',
+      test_string: 'test string'
+    })
+  })
+
+  it('fails if no item is provided', () => {
+    const invalidCall = () => TestEntity.build(BatchPutItemRequest).params()
+
+    expect(invalidCall).toThrow(DynamoDBToolboxError)
+    expect(invalidCall).toThrow(expect.objectContaining({ code: 'operations.incompleteCommand' }))
+  })
+})
