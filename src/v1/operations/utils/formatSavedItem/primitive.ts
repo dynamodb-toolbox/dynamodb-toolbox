2da26cc157e452ae8755ce6b6c90c851e2ff8c11
ThomasAribart <thomasa@theodo.fr>
2023-12-24 18:07:30 +0100

    use format option while formatting items

--- src/v1/operations/utils/formatSavedItem/primitive.ts
+++ src/v1/operations/utils/formatSavedItem/primitive.ts
@@ -2,39 +2,24 @@ import type {
   PrimitiveAttribute,
   AttributeValue,
   PrimitiveAttributeValue,
-  ResolvedPrimitiveAttribute
+  ResolvedPrimitiveAttribute,
+  Transformer
 } from 'v1/schema'
 import { validatorsByPrimitiveType } from 'v1/utils/validation'
 import { DynamoDBToolboxError } from 'v1/errors'
 
 export const parseSavedPrimitiveAttribute = (
   primitiveAttribute: PrimitiveAttribute,
-  savedPrimitive: AttributeValue
+  _savedPrimitive: AttributeValue
 ): PrimitiveAttributeValue => {
   const validator = validatorsByPrimitiveType[primitiveAttribute.type]
-  if (!validator(savedPrimitive)) {
+  if (!validator(_savedPrimitive)) {
     throw new DynamoDBToolboxError('operations.formatSavedItem.invalidSavedAttribute', {
       message: `Invalid attribute in saved item: ${primitiveAttribute.path}. Should be a ${primitiveAttribute.type}`,
       path: primitiveAttribute.path,
       payload: {
-        received: savedPrimitive,
+        received: _savedPrimitive,
         expected: primitiveAttribute.type
-      }
-    })
-  }
-
-  if (
-    primitiveAttribute.enum !== undefined &&
-    !primitiveAttribute.enum.includes(savedPrimitive as ResolvedPrimitiveAttribute)
-  ) {
-    throw new DynamoDBToolboxError('operations.formatSavedItem.invalidSavedAttribute', {
-      message: `Invalid attribute in saved item: ${
-        primitiveAttribute.path
-      }. Should be one of: ${primitiveAttribute.enum.map(String).join(', ')}`,
-      path: primitiveAttribute.path,
-      payload: {
-        received: savedPrimitive,
-        expected: primitiveAttribute.enum
       }
     })
   }
@@ -42,5 +27,23 @@ export const parseSavedPrimitiveAttribute = (
   /**
    * @debt type "validator should act as typeguard"
    */
-  return savedPrimitive as ResolvedPrimitiveAttribute
+  const savedPrimitive = _savedPrimitive as ResolvedPrimitiveAttribute
+  const transformer = primitiveAttribute.transform as Transformer
+  const formattedValue =
+    transformer !== undefined ? transformer.format(savedPrimitive) : savedPrimitive
+
+  if (primitiveAttribute.enum !== undefined && !primitiveAttribute.enum.includes(formattedValue)) {
+    throw new DynamoDBToolboxError('operations.formatSavedItem.invalidSavedAttribute', {
+      message: `Invalid attribute in saved item: ${
+        primitiveAttribute.path
+      }. Should be one of: ${primitiveAttribute.enum.map(String).join(', ')}`,
+      path: primitiveAttribute.path,
+      payload: {
+        received: formattedValue,
+        expected: primitiveAttribute.enum
+      }
+    })
+  }
+
+  return formattedValue
 }
