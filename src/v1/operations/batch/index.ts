323f879f66d01e0d70be35c465bb6f3747483820
Guillaume Duboc <guillaumed@theodo.fr>
2024-01-12 15:37:12 +0100

    feat: define batchWrite helper

---
+++ src/v1/operations/batch/index.ts
@@ -0,0 +1,5 @@
+export type { BatchWriteItemRequest } from './BatchWriteItemRequest'
+export type { BatchWriteOptions } from './options'
+export { batchWrite } from './batchWrite'
+export { BatchDeleteItemRequest } from './deleteItem'
+export { BatchPutItemRequest } from './putItem'
