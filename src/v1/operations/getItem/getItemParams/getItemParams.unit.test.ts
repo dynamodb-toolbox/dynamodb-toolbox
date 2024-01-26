77420a37b5ad176d1762b75e73543400afb042b0
Guillaume Duboc <guillaumed@theodo.fr>
2024-01-26 18:06:05 +0100

    chore: remove document client from unit tests

--- src/v1/operations/getItem/getItemParams/getItemParams.unit.test.ts
+++ src/v1/operations/getItem/getItemParams/getItemParams.unit.test.ts
@@ -1,11 +1,4 @@
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
-import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb'
-
 import { TableV2, EntityV2, schema, string, DynamoDBToolboxError, GetItemCommand, prefix } from 'v1'
-
-const dynamoDbClient = new DynamoDBClient({})
-
-const documentClient = DynamoDBDocumentClient.from(dynamoDbClient)
 
 const TestTable = new TableV2({
   name: 'test-table',
@@ -16,8 +9,7 @@ const TestTable = new TableV2({
   sortKey: {
     type: 'string',
     name: 'sk'
-  },
-  documentClient
+  }
 })
 
 const TestEntity = new EntityV2({
@@ -33,8 +25,7 @@ const TestEntity = new EntityV2({
 const TestTable2 = new TableV2({
   name: 'test-table',
   partitionKey: { type: 'string', name: 'pk' },
-  sortKey: { type: 'string', name: 'sk' },
-  documentClient
+  sortKey: { type: 'string', name: 'sk' }
 })
 
 const TestEntity2 = new EntityV2({
