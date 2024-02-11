71c3e67afb5d58411cbaa3d67c0bb5a37ff77651
ThomasAribart <thomasa@theodo.fr>
2024-02-11 18:27:33 +0100

    update parseSchema to Parser class

--- src/v1/operations/utils/parseKeyInput.ts
+++ src/v1/operations/utils/parseKeyInput.ts
@@ -1,21 +1,21 @@
 import type { EntityV2 } from 'v1/entity'
 import type { Item, RequiredOption } from 'v1/schema'
-import { schemaParser } from 'v1/parser'
+import { Parser } from 'v1/parser'
 
 type EntityKeyInputParser = (entity: EntityV2, input: Item) => Generator<Item, Item>
 
 const requiringOptions = new Set<RequiredOption>(['always'])
 
 export const parseEntityKeyInput: EntityKeyInputParser = (entity, input) => {
-  const parser = schemaParser(entity.schema, input, {
+  const workflow = entity.schema.build(Parser).workflow(input, {
     fill: 'key',
     transform: true,
     filters: { key: true },
     requiringOptions
   })
 
-  parser.next() // defaulted
-  parser.next() // linked
+  workflow.next() // defaulted
+  workflow.next() // linked
 
-  return parser
+  return workflow
 }
