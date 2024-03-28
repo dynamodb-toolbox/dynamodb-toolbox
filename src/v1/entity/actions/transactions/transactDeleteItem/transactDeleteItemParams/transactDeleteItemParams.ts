12a5fae1fe595678f3cc7feb3c8fbf8125a65c13
Thomas Aribart <thomas.aribart.ext@axaclimate.com>
2024-03-28 18:37:20 +0100

    create entityTParser and move KeyInput type there

--- src/v1/entity/actions/transactions/transactDeleteItem/transactDeleteItemParams/transactDeleteItemParams.ts
+++ src/v1/entity/actions/transactions/transactDeleteItem/transactDeleteItemParams/transactDeleteItemParams.ts
@@ -2,7 +2,7 @@ import type { TransactWriteCommandInput } from '@aws-sdk/lib-dynamodb'
 
 import type { EntityV2 } from 'v1/entity'
 import { EntityParser } from 'v1/entity/actions/parse'
-import type { KeyInput } from 'v1/operations/types'
+import type { KeyInput } from 'v1/entity/actions/tParse'
 
 import type { DeleteItemTransactionOptions } from '../options'
 import { parseDeleteItemTransactionOptions } from './parseDeleteItemOptions'
