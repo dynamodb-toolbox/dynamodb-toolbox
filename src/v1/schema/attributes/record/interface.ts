e68756ec370f989f52182f819132cac6a4c0d966
ThomasAribart <thomasa@theodo.fr>
2024-04-23 00:50:13 +0200

    fix issue with build method on attributes

--- src/v1/schema/attributes/record/interface.ts
+++ src/v1/schema/attributes/record/interface.ts
@@ -350,9 +350,11 @@ export class RecordAttribute<
     this.links = state.links
   }
 
-  build<SCHEMA_ACTION extends SchemaAction<this> = SchemaAction<this>>(
-    schemaAction: new (schema: this) => SCHEMA_ACTION
-  ): SCHEMA_ACTION {
-    return new schemaAction(this)
-  }
+  // DO NOT DE-COMMENT right now as they trigger a ts(7056) error on even relatively small schemas
+  // TODO: Find a way not to trigger this error
+  // build<SCHEMA_ACTION extends SchemaAction<this> = SchemaAction<this>>(
+  //   schemaAction: new (schema: this) => SCHEMA_ACTION
+  // ): SCHEMA_ACTION {
+  //   return new schemaAction(this)
+  // }
 }
