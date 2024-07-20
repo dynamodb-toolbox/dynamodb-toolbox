50edd675a7b046c2bb9a409b945175c6369db495
Naor Peled <thehecticbyte@gmail.com>
2024-07-20 20:16:51 +0300

    wip: resolve issues with ESM

--- src/types/optional.ts
+++ src/types/optional.ts
@@ -1,7 +1,5 @@
-import type { ComputeObject } from './computeObject.js'
-
-export type Optional<OBJECT extends object, KEYS extends string | number | symbol> = ComputeObject<
-  { [KEY in Exclude<keyof OBJECT, KEYS>]: OBJECT[KEY] } & {
-    [KEY in Extract<keyof OBJECT, KEYS>]?: OBJECT[KEY]
-  }
->
+export type Optional<OBJECT extends object, KEYS extends string | number | symbol> = {
+  [KEY in Exclude<keyof OBJECT, KEYS>]: OBJECT[KEY]
+} & {
+  [KEY in Extract<keyof OBJECT, KEYS>]?: OBJECT[KEY]
+}
---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  clean: true,
+  dts: true,
+  entry: ['./src/index.ts'],
+  format: ['cjs', 'esm'],
+  legacyOutput: true,
+  outDir: 'dist',
+  sourcemap: true
+})
