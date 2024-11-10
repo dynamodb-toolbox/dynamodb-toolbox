a33facf4602e3b018bd6202d16e44dc6dd496e70
ThomasAribart <thomasa@theodo.fr>
2024-11-10 16:45:48 +0100

    fix bug on formatter

---
+++ src/schema/actions/format/attribute.unit.test.ts
@@ -0,0 +1,66 @@
+import { string } from '~/attributes/index.js'
+import { DynamoDBToolboxError } from '~/errors/index.js'
+
+import * as primitiveAttrFormatterModule from './primitive.js'
+import { attrFormatter } from './attribute.js'
+
+const primitiveAttrFormatter = vi.spyOn(primitiveAttrFormatterModule, 'primitiveAttrFormatter')
+
+const strAttr = string().freeze('path')
+const optStrAttr = string().optional().freeze('path')
+
+describe('attrFormatter', () => {
+  beforeEach(() => {
+    primitiveAttrFormatter.mockClear()
+  })
+
+  test('throws an error if value is missing and required', () => {
+    const invalidCall = () => attrFormatter(strAttr, undefined).next()
+
+    expect(invalidCall).toThrow(DynamoDBToolboxError)
+    expect(invalidCall).toThrow(expect.objectContaining({ code: 'formatter.missingAttribute' }))
+  })
+
+  test('returns/yields undefined if value is missing and not required', () => {
+    const formatter = attrFormatter(optStrAttr, undefined)
+
+    const { value: transformedValue } = formatter.next()
+    expect(transformedValue).toBeUndefined()
+
+    const { done, value: formattedValue } = formatter.next()
+    expect(done).toBe(true)
+    expect(formattedValue).toBeUndefined()
+  })
+
+  test('applies expected formatter on input otherwise (and pass options)', () => {
+    const options = { some: 'options' }
+    const formatter = attrFormatter(
+      strAttr,
+      'foo',
+      // @ts-expect-error we don't really care about the type here
+      options
+    )
+
+    const { value: transformedValue } = formatter.next()
+    expect(transformedValue).toStrictEqual('foo')
+
+    expect(primitiveAttrFormatter).toHaveBeenCalledOnce()
+    expect(primitiveAttrFormatter).toHaveBeenCalledWith(strAttr, 'foo', options)
+
+    const { done, value: formattedValue } = formatter.next()
+    expect(done).toBe(true)
+    expect(formattedValue).toStrictEqual('foo')
+  })
+
+  test('does not transform if transform is false', () => {
+    const options = { transform: false }
+    const formatter = attrFormatter(strAttr, 'foo', options)
+
+    const { done, value: formattedValue } = formatter.next()
+    expect(done).toBe(true)
+    expect(formattedValue).toStrictEqual('foo')
+
+    expect(primitiveAttrFormatter).toHaveBeenCalledOnce()
+    expect(primitiveAttrFormatter).toHaveBeenCalledWith(strAttr, 'foo', options)
+  })
+})
